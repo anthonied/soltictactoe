c2fd2c1f7b4ef8b894fea177c68f11fb1c448167
Anthonie <anthonied@solutionserver.co.za>
2014-09-05 10:42:13 +0200

    first build

--- TicTacToeKata/SolTicTacToe.Domain/Game.cs
+++ TicTacToeKata/SolTicTacToe.Domain/Game.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace SolTicTacToe.Domain
 {
-    public class Game
+    public class Game : ITicTacToe
     {
+        public Coordinate MakeMove(Board CurrentBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ITicTacToe
+    {
+        Coordinate MakeMove(Board CurrentBoard);
+    }
+
+    public class Board
+    {
+        public Dictionary<Coordinate, string> CurrentBoard { get; set; }
+    }
+
+    public class Coordinate
+    {
+        private int _x { get; set; }
+        public int _y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
     }
 }
