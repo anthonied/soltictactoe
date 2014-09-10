70edb0e521d5d3d657a35c8f45eb2a9154b09e53
Gerhard Hattingh <gerhardh@solutionserver.co.za>
2014-09-10 10:05:50 +0200

    Update to domain's classes, with named string types

--- TicTacToeKata/SolTicTacToe.Domain/Game.cs
+++ TicTacToeKata/SolTicTacToe.Domain/Game.cs
@@ -1,48 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CoordinateKey = System.String;
+using Mark = System.String;
 
 namespace SolTicTacToe.Domain
 {
 	//public class Game : ITicTacToe
 	//{
-	//	public Coordinate MakeMove(Board currentBoard, string markToMake)
+	//	public Coordinate MakeMove(Board currentBoard, Mark markToMake)
 	//	{
 	//		throw new NotImplementedException();
 	//	}
 	//}
 
-	public interface ITicTacToe
-	{
-		Coordinate MakeMove(Board currentBoard, string markToMake);
-	}
+    public interface ITicTacToe
+    {
+		Coordinate MakeMove(Board currentBoard, Mark markToMake);
+    }
 
-	public class Board
-	{
+    public class Board
+    {
 		public Board()
 		{
-			Moves = new Dictionary<string, string>();
+			Moves = new Dictionary<CoordinateKey, Mark>();
 		}
 
-		public Dictionary<string, string> Moves { get; set; }
-	}
+		public Dictionary<CoordinateKey, Mark> Moves { get; set; }
+    }
 
-	public class Coordinate
-	{
-		public int X { get; private set; }
-		public int Y { get; private set; }
+    public class Coordinate
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
-		public Coordinate(int x, int y)
-		{
-			X = x;
-			Y = y;
-		}
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
 
-		public string Key
+		public CoordinateKey Key
 		{
 			get { return string.Format("{0},{1}", X, Y); }
 		}
-	}
+    }
 }
